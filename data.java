08a37634492b4c3f0998b9a49cb01c9ffba34357
Cukiero <mat.cichocki95@gmail.com>
2017-05-24 18:31:26 +0200

    Add files via upload

---
+++ data.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.linkmanager;
+
+/**
+ *
+ * @author Cukier
+ */
+public class data {
+    int id;
+    String name;
+    String adress;
+    public data(int id, String name, String adress){
+        this.id=id;
+        this.name=name;
+        this.adress=adress;
+    }
+}
---
+++ myjButton.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.linkmanager;
+
+/**
+ *
+ * @author Cukier
+ */
+public class myjButton extends javax.swing.JButton {
+    public String adress;
+    public myjButton(String text){
+        super(text);
+    }
+}
