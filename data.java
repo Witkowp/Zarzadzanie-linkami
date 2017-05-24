5f3b56c519a5f189ff4227f2e8d7b092a677bae7
Cukiero <mat.cichocki95@gmail.com>
2017-05-24 18:56:51 +0200

    Update data.java

--- data.java
+++ data.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package my.linkmanager;
 
 /**
@@ -13,9 +8,12 @@ public class data {
     int id;
     String name;
     String adress;
-    public data(int id, String name, String adress){
+    String direct;
+    public data(int id, String name, String adress, String direct){
         this.id=id;
         this.name=name;
         this.adress=adress;
+        this.direct=direct;
+        
     }
 }
