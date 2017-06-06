6853c615279cce2c83c4ed9e21b35174b8d4edcf
RozanovKa <kateryna.rozanova@gmail.com>
2017-06-06 20:16:46 +0200

    nowy plik:     src/my/addfolder.png 	nowy plik:     src/my/addfolder2.png 	nowy plik:     src/my/addfolderchosen.png 	nowy plik:     src/my/addicon.png 	nowy plik:     src/my/addlink.png 	nowy plik:     src/my/addlinkmouse.png 	nowy plik:     src/my/addlinkpress.png 	nowy plik:     src/my/backfolder.png 	nowy plik:     src/my/backfolder2.png 	nowy plik:     src/my/backfolderchosen.png 	nowy plik:     src/my/deleteicon.png 	nowy plik:     src/my/deletemouseicon.png 	nowy plik:     src/my/deletepressicon.png 	nowy plik:     src/my/folder.png 	nowy plik:     src/my/linkbutt.png 	nowy plik:     src/my/linkbuttmouse.png 	nowy plik:     src/my/linkbuttpress.png 	nowy plik:     src/my/linkmanager/BazaDanych.java 	nowy plik:     src/my/linkmanager/Data.java 	nowy plik:     src/my/linkmanager/LinkManagerUI.java 	nowy plik:     src/my/linkmanager/MyjButton.java 	nowy plik:     src/my/linkmanager/Test.java 	nowy plik:     src/my/linkmanager/TitleExtractor.java 	nowy plik:     src/my/openfolder.png 	nowy plik:     src/my/openfolder1.png 	nowy plik:     src/my/syncicon.png 	nowy plik:     src/my/syncmouseicon.png 	nowy plik:     src/my/syncpressicon.png 	nowy plik:     test/my/linkmanager/BazaDanychTest.java

---
+++ src/my/linkmanager/BazaDanych.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.linkmanager;
+
+/**
+ *
+ * @author Cukier
+ */
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Driver;
+import com.mysql.jdbc.Statement;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ *
+ * @author Piotr
+ */
+public class BazaDanych {
+    
+
+
+
+    public static List <Data> getFromSQL(String tableName) throws SQLException, IOException, URISyntaxException {//pobieranie z bazy danych
+        List<Data> links = new LinkedList<>();
+        try (Connection conn = getConnection()) {
+            Statement stat = (Statement) conn.createStatement();
+            try (ResultSet result = stat.executeQuery("SELECT * FROM "+tableName);) {
+                while (result.next()) {
+                    long id=Integer.parseInt(result.getString("urlId"));
+                    String url=result.getString("url");
+                    String name=result.getString("nazwa");
+                    String folder=result.getString("folder");
+                    Data data= new Data(id,name,url,folder);
+                    System.out.println(data);
+                    links.add(data);
+                }
+            }
+        }
+        System.out.println(links);
+        return links;
+    }
+
+    public static long insertIntoSQL(String tableName, String url, String nazwa,String sciezkaDoPliku) throws SQLException {//wstawianie do bazy danych
+        long numer=0;
+        try (Connection conn = getConnection()) {
+            Statement stat = (Statement) conn.createStatement();
+            stat.executeUpdate("INSERT INTO " + tableName + " VALUES(NULL,'" + url + "' , '" + nazwa + "', '" + sciezkaDoPliku + "')", Statement.RETURN_GENERATED_KEYS);
+            ResultSet generatedKeys = stat.getGeneratedKeys();
+            if (generatedKeys.next()) {
+            return generatedKeys.getLong(1);
+            }else{
+                throw new SQLException("Brak id");
+            }
+            
+        }
+    }
+    public static void deleteFromSQL(String nazwaTabeli, long urlid) throws SQLException{//usuwanie z bazy danych
+        try(Connection conn=getConnection()){
+            Statement stat= (Statement) conn.createStatement();
+            stat.executeUpdate("DELETE FROM " + nazwaTabeli+ " WHERE urlid="+urlid);
+        }
+    
+    }
+
+    public static Connection getConnection() throws SQLException {//ustawienie polaczenia z baza danych
+        String url = "jdbc:mysql://45.77.55.209:3306/zarzadzanie";
+        Properties prop = new Properties();
+        prop.setProperty("user", "piopio");
+        prop.setProperty("password", "pw");
+        Driver d = new com.mysql.jdbc.Driver();
+        Connection con = (Connection) d.connect(url, prop);
+        System.out.println(con);
+        return con;
+    }
+
+
+}
